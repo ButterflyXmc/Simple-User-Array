1e8fcb050b1b0794dd129173f02907617aa107bc
mahic <mahic@domain.com>
2022-11-06 22:09:19 -0700

    done

---
+++ app.js
@@ -0,0 +1,13 @@
+let user_names = [`Olivia`, `Emma`, `Liam`, `Elijah`, `Mia`, `William`, `Isabella`, `Theodore`, `Ava`, `Sophia`];
+let user_ages = [20, 15, 19, 14, 17, 20, 22, 16, 10, 23];
+
+
+let counter = 0;
+while (user_ages[counter] >= 18){
+    console.log(user_names);
+    counter++;
+}
+
+for (let i = 0; user_ages[i] < 18; counter++){
+    console.log(user_names);
+}
